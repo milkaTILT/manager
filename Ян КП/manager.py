cfd58a37425caef8d8101f6f99abedba69c8505b
milkaTILT <128968400+milkaTILT@users.noreply.github.com>
2023-05-21 13:30:23 +0300

    Update manager.py

--- Ян КП/manager.py
+++ Ян КП/manager.py
@@ -1,4 +1,7 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
+import json
+import keyring
+# idkru.pythonanywhere.com/code/dezine
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
@@ -8,13 +11,18 @@ class MainWindow(QtWidgets.QWidget):
         self.setWindowTitle('PasswordManager')
         self.resize(600, 480)
 
+        self.db = DataBase()
+
+        self.add_pwd_win = PasswordWindow(self.db, self)
+        self.btn_add_pwd.clicked.connect(self.add_pwd_win.show)
+
     def initUI(self):
         self.btn_add_pwd = QtWidgets.QPushButton('Добавить')
         self.pwd_list = QtWidgets.QListWidget()
 
         h_layout = QtWidgets.QHBoxLayout()
         v_layout = QtWidgets.QVBoxLayout()
-        
+
         h_layout.addStretch(6)
         h_layout.addWidget(self.btn_add_pwd, stretch=2)
 
@@ -23,10 +31,20 @@ class MainWindow(QtWidgets.QWidget):
 
         self.setLayout(v_layout)
 
+    def update_list(self):
+        self.pwd_list.clear()
+        data = self.db.get_list()
+        self.pwd_list.addItems(data)
+
 
 class PasswordWindow(QtWidgets.QWidget):
-    def __init__(self, parent):
-        super().__init__(parent)
+    def __init__(self, db, mw):
+        super().__init__()
+        self.db = db
+        self.mw = mw
+        self.initUI()
+
+        self.btn_save.clicked.connect(self.add_password)
 
     def initUI(self):
         v_layout = QtWidgets.QVBoxLayout()
@@ -34,8 +52,8 @@ class PasswordWindow(QtWidgets.QWidget):
         h_1 = QtWidgets.QHBoxLayout()
         h_2 = QtWidgets.QHBoxLayout()
         h_3 = QtWidgets.QHBoxLayout()
-
-        self.btn_edit = QtWidgets.QPushButton('Изменить')
+        
+        self.btn_save = QtWidgets.QPushButton('Сохранить')
         self.btn_close = QtWidgets.QPushButton('Закрыть')
         self.btn_copy = QtWidgets.QPushButton('Copy')
 
@@ -61,8 +79,47 @@ class PasswordWindow(QtWidgets.QWidget):
         v_layout.addLayout(h_1)
         v_layout.addLayout(h_2)
         v_layout.addLayout(h_3)
+        v_layout.addWidget(self.btn_save, alignment=QtCore.Qt.AlignCenter)
 
         self.setLayout(v_layout)
+
+    def add_password(self):
+        service = self.service_edit.text()
+        login = self.login_edit.text()
+        password = self.pwd_edit.text()
+        self.db.set_password(service, login, password)
+        self.service_edit.clear()
+        self.login_edit.clear()
+        self.pwd_edit.clear()
+        self.hide()
+        self.mw.update_list()
+
+
+class DataBase:
+    def __init__(self):
+        self.filename = 'services.json'
+        try:
+            with open(self.filename, 'r') as file:
+                self.data = json.load(file)
+        except:
+            open(self.filename, 'w')
+            self.data = dict()
+
+    def set_password(self, service, login, password):
+        keyring.set_password(service, login, password)
+        self.data[service] = login
+        with open(self.filename, 'w') as file:
+            json.dump(self.data, file)
+        
+    def get_password(self, service, login):
+        return keyring.get_password(service, login)
+
+    def get_list(self):
+        result = []
+        for key in self.data:
+            text = key + ' | ' + self.data[key]
+            result.append(text)
+        return result
 
 
 if __name__ == '__main__':
@@ -70,3 +127,7 @@ if __name__ == '__main__':
     mw = MainWindow()
     mw.show()
     app.exec()
+
+
+# https://ibb.co/bBZW4pH
+# https://ibb.co/b6qHmKd
